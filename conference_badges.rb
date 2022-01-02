72aaa7b6a9a706eaea7627915444b27b7cbf63c3
Chris Ninman <ctsnackpack@gmail.com>
2022-01-02 11:19:45 +0100

    commit tree b3764209365215df04f5a97402e376c1cf60d204

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+require "pry"
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(array)
+  array.map do |name|
+    index = array.find_index (name)
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+assign_rooms([ "a", "b", "c" ])
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  badges.map do |attendee|
+    puts attendee
+  end
+  rooms = assign_rooms(attendees)
+  rooms.map do |room|
+    puts room
+  end
+end
+
